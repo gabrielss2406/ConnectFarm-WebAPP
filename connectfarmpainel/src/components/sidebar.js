560e323ad3905d6fcf8372bcc53f6d1bb9e5ba93
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-01-12 11:37:01 -0300

    feat: siidebar as component

---
+++ connectfarmpainel/src/components/sidebar.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Logo from '../static/logo.png';
+import DataAnalisys from '../static/dataAnalisys.png';
+import DataAnalisysGray from '../static/dataAnalisys_gray.png';
+import DashBoard from '../static/dashboard.png';
+import DashBoardGray from '../static/dashboard_gray.png';
+import Performance from '../static/performance.png';
+import PerformanceGray from '../static/performance_gray.png';
+
+function Sidebar() {
+  const [isDataAnalisysHovered, setIsDataAnalisysHovered] = useState(false);
+  const [isPerformanceHovered, setIsPerformanceHovered] = useState(false);
+  const [isDashBoardHovered, setIsDashBoardHovered] = useState(false);
+
+  return (
+    <div className="sidebar">
+      <div className="title">
+        <img className="imgLogo" src={Logo} alt="connectFarm-logo" />
+        <h1>ConnectFarm</h1>
+      </div>
+      <div className="options">
+        <div className="options-title">
+          <h2>OPTIONS</h2>
+        </div>
+        
+        <div className="menu">
+          <button
+            onMouseEnter={() => setIsDataAnalisysHovered(true)}
+            onMouseLeave={() => setIsDataAnalisysHovered(false)}
+          >
+            <img
+              className="imgOptions"
+              src={isDataAnalisysHovered ? DataAnalisys : DataAnalisysGray}
+              alt="connectFarm-logo"
+            />
+            DataAnalisys
+          </button>
+          <button
+            onMouseEnter={() => setIsPerformanceHovered(true)}
+            onMouseLeave={() => setIsPerformanceHovered(false)}
+          >
+            <img
+              className="imgOptions"
+              src={isPerformanceHovered ? Performance : PerformanceGray}
+              alt="connectFarm-logo"
+            />
+            Performance
+          </button>
+          <button
+            onMouseEnter={() => setIsDashBoardHovered(true)}
+            onMouseLeave={() => setIsDashBoardHovered(false)}
+          >
+            <img
+              className="imgOptions"
+              src={isDashBoardHovered ? DashBoard : DashBoardGray}
+              alt="connectFarm-logo"
+            />
+            DashBoard
+          </button>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
--- connectfarmpainel/src/project/main/Main.js
+++ connectfarmpainel/src/project/main/Main.js
@@ -1,69 +1,11 @@
 import './Main.css';
-import Logo from '../../static/logo.png';
-import DataAnalisys from '../../static/dataAnalisys.png';
-import DataAnalisysGray from '../../static/dataAnalisys_gray.png';
-import DashBoard from '../../static/dashboard.png';
-import DashBoardGray from '../../static/dashboard_gray.png';
-import Performance from '../../static/performance.png';
-import PerformanceGray from '../../static/performance_gray.png';
-
-import React, { useState } from 'react';
+import React from 'react';
+import Sidebar from '../../components/sidebar'; 
 
 function Main() {
-  const [isDataAnalisysHovered, setIsDataAnalisysHovered] = useState(false);
-  const [isPerformanceHovered, setIsPerformanceHovered] = useState(false);
-  const [isDashBoardHovered, setIsDashBoardHovered] = useState(false);
-
   return (
     <div className="screen">
-      <div className="sidebar">
-        <div className="title">
-          <img className="imgLogo" src={Logo} alt="connectFarm-logo" />
-          <h1>ConnectFarm</h1>
-        </div>
-        <div className="options">
-          <div className="options-title">
-            <h2>OPTIONS</h2>
-          </div>
-          
-          <div className="menu">
-            <button
-              onMouseEnter={() => setIsDataAnalisysHovered(true)}
-              onMouseLeave={() => setIsDataAnalisysHovered(false)}
-            >
-              <img
-                className="imgOptions"
-                src={isDataAnalisysHovered ? DataAnalisys : DataAnalisysGray}
-                alt="connectFarm-logo"
-              />
-              DataAnalisys
-            </button>
-            <button
-              onMouseEnter={() => setIsPerformanceHovered(true)}
-              onMouseLeave={() => setIsPerformanceHovered(false)}
-            >
-              <img
-                className="imgOptions"
-                src={isPerformanceHovered ? Performance : PerformanceGray}
-                alt="connectFarm-logo"
-              />
-              Performance
-            </button>
-            <button
-              onMouseEnter={() => setIsDashBoardHovered(true)}
-              onMouseLeave={() => setIsDashBoardHovered(false)}
-            >
-              <img
-                className="imgOptions"
-                src={isDashBoardHovered ? DashBoard : DashBoardGray}
-                alt="connectFarm-logo"
-              />
-              DashBoard
-            </button>
-          </div>
-          
-        </div>
-      </div>
+      <Sidebar />
       <div className="main-content">
         <div className="content-title"><h1>Data Analisys</h1></div>
         <div className="content"></div>
