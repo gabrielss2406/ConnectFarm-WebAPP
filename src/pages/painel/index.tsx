12823517eaf5487b07538080b639f545e80d761c
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-08-05 16:49:31 -0300

    fix: background from painel

--- src/pages/painel/index.tsx
+++ src/pages/painel/index.tsx
@@ -32,18 +32,18 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col h-full bg-[#F1F1F1] sm:ml-[15%]"> {/* Fundo da tela */}
-      <div className="p-5 bg-white flex w-full items-center justify-between border-b border-gray-200"> {/* Cabeçalho */}
-        <h1 className="text-black text-[14pt] font-bold">Data Analysis</h1> {/* Cor do texto do cabeçalho */}
+    <div className="flex flex-col min-h-screen bg-[#F1F1F1] sm:ml-[15%]">
+      <header className="p-5 bg-white flex w-full items-center justify-between">
+        <h1 className="text-black text-[14pt] font-bold">Data Analysis</h1>
         <SelectFarm farms={farms} activeFarm={activeFarm} setActiveFarm={setActiveFarm} />
-      </div>
-      <div className="flex w-full h-auto flex-wrap justify-around gap-4 pt-4 flex-row p-10">
+      </header>
+      <main className="flex-grow flex flex-col w-full h-auto flex-wrap justify-around gap-4 pt-4 flex-row p-10">
         <InformationExpenses expenseType="Lucro" amount={45.67} invertColor={false} />
         <InformationExpenses expenseType="Lucro" amount={45.67} invertColor={true} />
         <InformationExpenses expenseType="Lucro" amount={45.67} invertColor={false} />
         <InformationExpenses expenseType="Lucro" amount={45.67} invertColor={true} />
-      </div>
-      <div className="flex flex-col items-center gap-4 p-10">
+      </main>
+      <footer className="flex flex-col items-center gap-4 p-10">
         <Chart
           options={chartOptions}
           series={chartSeries}
@@ -62,7 +62,7 @@ export default function Home() {
           type="donut"
           width="380"
         />
-      </div>
+      </footer>
     </div>
   );
 }
