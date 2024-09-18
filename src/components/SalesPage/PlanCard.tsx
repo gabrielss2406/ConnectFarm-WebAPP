a69777fb044330da81051137a46e99f749c3f4a9
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-09-18 10:38:56 -0300

    feat: ours plans

--- src/components/SalesPage/PlanCard.tsx
+++ src/components/SalesPage/PlanCard.tsx
@@ -14,6 +14,9 @@ export const PlanCard: React.FC<PlanCardProps> = ({ plan, selectedPlan, onSelect
         <div className="bg-[#1e1e1e] text-white p-6 rounded-lg shadow-lg flex flex-col items-center">
             <h2 className="text-2xl font-semibold mb-4">{plan.name}</h2>
             <p className="text-lg font-bold mb-4">{plan.price}</p>
+            <p className="text-sm text-gray-400 mb-4 text-center">
+                {plan.description}
+            </p>
             <ul className="text-left space-y-2 mb-6">
                 {plan.features.map((feature, index) => (
                     <li key={index} className="flex items-center">
--- src/components/SalesPage/PlansList.tsx
+++ src/components/SalesPage/PlansList.tsx
@@ -4,6 +4,7 @@ import { PlanCard } from './PlanCard';
 export interface Plan {
     id: number;
     name: string;
+    description: string
     price: string;
     features: string[];
 }
@@ -11,35 +12,37 @@ export interface Plan {
 const plans: Plan[] = [
     {
         id: 1,
-        name: 'Plano Básico',
+        name: 'Plano inicial',
+        description: 'Ideal para pequenos produtores que precisam de mais organização',
         price: 'R$ 19,90/mês',
         features: [
-            'Proteção para 1 dispositivo',
-            'Suporte básico',
+            'Ferramentas de gestão do aplicativo',
+            'Painel Web para visualização dos dados',
         ],
     },
     {
         id: 2,
         name: 'Plano Avançado',
+        description: 'Ideal para os que querem entender melhor seu negócio sem gastar com infraestrutura',
         price: 'R$ 49,90/mês',
         features: [
-            'Proteção para 1 dispositivo',
-            'Suporte básico',
-            'Proteção para até 5 dispositivos',
-            'Relatórios de segurança detalhados',
+            'Ferramentas de gestão do aplicativo',
+            'Painel Web para visualização dos dados',
+            'Painel Web para analises preditivas',
+            'Painel Web para correlação de dados'
         ],
     },
     {
         id: 3,
         name: 'Plano Premium',
+        description: 'Ideal para os que precisam juntar uma coleta mais automatizada com analises completas do negócio',
         price: 'R$ 99,90/mês',
         features: [
-            'Proteção para 1 dispositivo',
-            'Suporte básico',
-            'Proteção para até 5 dispositivos',
-            'Relatórios de segurança detalhados',
-            'Proteção familiar (até 10 dispositivos)',
-            'Consultoria de segurança personalizada',
+            'Ferramentas de gestão do aplicativo',
+            'Painel Web para visualização dos dados',
+            'Painel Web para analises preditivas',
+            'Painel Web para correlação de dados',
+            'Integração com hardware'
         ],
     },
 ];
